175c40fa7d2cb7b2e48efe5a4a5421de1302320c
Aleksandra Chrzanowska <aleksandra.chrzanowska@student.uj.edu.pl>
2019-10-16 23:43:13 +0200

    dodane pliki z sortowaniem (sprawdzić czy wszystkie działają)

---
+++ Selectionsort.cpp
@@ -0,0 +1,24 @@
+//
+// Created by ola on 28.08.19.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+
+    std::vector<int> v = {8, 1, 5, 2, 4};
+
+    for(int i = 0; i < v.size(); i++){
+        for(int j = 0; j < v.size(); j++){
+            if(v[i] < v[j]){
+                std::swap(v[i], v[j]);
+            }
+        }
+    }
+    for(int i = 0; i < v.size(); i++){
+        std::cout << v[i] << "\t";
+    }
+    std::cout << std::endl;
+}
