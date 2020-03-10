bc653ae0da22d4a151d26c5e75abe3a9936a3083
Aleksandra Chrzanowska <aleksandra.chrzanowska@student.uj.edu.pl>
2020-03-10 15:07:50 +0100

    Poprwa SelectionSort

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,33 +1,3 @@
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-
-// void selectionSort(std::vector<int> list){
-//     for(int i = 0; i < list.size(); i++){
-//         int minEl = i;
-//         for(int j = i + 1; j < list.size(); j++){
-//             if(minEl > list[j]){
-//                 minEl = list[j];
-//             }
-//         }
-//         std::swap(list[i], minEl);
-//     }
-// }
-
-// int main(){
-//     std::vector<int> list;
-    
-//     for(int i = 0; i < 5; i++){
-//         int x;
-//         std::cin >> x;
-//         list.push_back(x); 
-//     }
-//     selectionSort(list);
-//     for(auto i: list){
-//         std::cout << i;
-//     }
-//     return 0;
-// }
 #include <iostream>
 #include <vector>
 #include <algorithm>
